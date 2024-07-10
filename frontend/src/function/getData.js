e229d9220e8e03b8e3dc782fd106ee5a29dd1721
yee0328623 <c109193208@nkust.edu.tw>
2024-07-10 10:25:44 +0800

    refactor 新增userAPI

---
+++ frontend/src/function/getData.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+const DataUrl = `${process.env.REACT_APP_localhost}/api/v1/user/`;
+//由前台去傳送要get，post，patch，delete，資料希望能塞進一個一維陣列，看需要甚麼資料直接取出來。
+export const userFunction = async (requestType,username, password) => {
+    try{
+        let response;
+        if(requestType === "post"){
+            response = await axios.post(`${DataUrl}`, {
+                username: username,
+                password: password
+        });
+    }else if(requestType === "get"){
+        response = await axios.get(`${DataUrl}`, {
+            headers: {
+                Authorization: `Bearer ${username}`,
+            }
+        });
+    }else if(requestType === "patch"){
+        response = await axios.post(`${DataUrl}`, {}, {
+            headers: {
+                'X-CSRF-Token': username
+            }
+        });
+    }else if(requestType === "delete"){
+        response = await axios.delete(`${DataUrl}`, {
+            headers: {
+                Authorization: `Bearer ${username}`,
+            }
+        });
+    }
+    return response.data;
+    }catch (error) {
+        console.error('Error fetching the data:', error);
+        return null;
+    }
+    //   try {
+//     const response = await axios.post(`${DataUrl}/login`, {
+//       username: username,
+//       password: password
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching the data:', error);
+//     return null;
+//   }
+}
